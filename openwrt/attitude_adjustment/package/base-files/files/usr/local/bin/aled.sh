f19dc8c79f0030f8fbefaea0c671ea4f54ef5c36
sp2ong <sp2ong@wp.pl>
2015-04-21 15:57:01 +0200

    Update aled.sh scanner and dhcp dns entry

--- openwrt/attitude_adjustment/package/base-files/files/usr/local/bin/aled.sh
+++ openwrt/attitude_adjustment/package/base-files/files/usr/local/bin/aled.sh
@@ -13,7 +13,7 @@ M=0
 h=0  
 P=1
 B="\t BRAK STACJI AKUTALNIE POLACZONYCH  \n\n "
-Ka=" Kierunek anteny - poziom SNR "
+Ka="\t \t Poziom SNR "
 # ilosc odczytow 100
 R=100
       if [ -z $HardN ]
@@ -28,7 +28,7 @@ R=100
        then
       h=1
 clear
-echo -e "\n   TO NIE JEST UBIQUITI - NIE BEDZIE SYGNALIZACJI LED-em"
+echo -e "\n \33[31;40;1m   TO NIE JEST UBIQUITI - NIE BEDZIE SYGNALIZACJI LED-em \e[0m"
 sleep 3
         fi
      if [ $h = 0 ]
@@ -42,17 +42,17 @@ sleep 3
 # szybkie blyski =  SNR < 150    silny sygnal
 #
 clear
-echo -e "\n--------------------------------------------------------------"
-echo " Wskaznik sygnalu do ustawienia kierunku anteny V1.09  SP9FUT"
+echo -e "\n\33[36;40m--------------------------------------------------------------"
+echo " Wskaznik sygnalu do ustawienia kierunku anteny V1.10  SP9FUT"
 echo "      STACJE POLACZONE BEZPOSREDNIO W SIECI HamNet "
-echo " `iwinfo $inter assoclist | grep "SNR"`"
-echo "--------------------------------------------------------------"
+echo "`iwinfo $inter assoclist | grep "SNR"`"
+echo -e "-------------------------------------------------------------- \e[0m"
 #
        if [ $N = 0 ]
          then   
     if  [ $HardN = "0777:C002" ]
          then
-echo -e "\t NanoStation2  sygnalizacja dioda LED"
+echo -e "\t\33[33;40m NanoStation2  sygnalizacja dioda LED \e[0m"
 l1="/sys/class/leds/gpio3/trigger"
 l2="/sys/class/leds/gpio3/delay_off"
 l3="/sys/class/leds/gpio3/delay_on"
@@ -76,27 +76,33 @@ Sa=$( echo $Wyn | cut -d " " -f1 )
 SNR=$((400 -((Snr) ** 2 )))
    if [ $SNR -le 2 ]
          then
-       SNR=2
+       SNR=1
    fi
    if [ $Snr -lt 0 ]
          then
     Snr=$((Snr * -1 ))
       fi 
-   if [ $SNR -le 350 -a $Snr > 0 ]
+   if [ $SNR -le 250 -a $Snr > 0 ]
       then
-echo -e "$Ka $Snr  odczyt - $P"
-# poziom sygnalu
+echo -e "\33[32;40m $Ka $Snr  odczyt - $P \e[0m"
 (echo "timer" > $l1)
 (echo "70" > $l2)
 (echo "$SNR" >  $l3)
-   elif [ $SNR -le 396 -a $Snr >0 ]
+      elif [ $SNR -le 350 -a $Snr > 0 ]
+      then
+echo -e "\33[33;40m $Ka $Snr  odczyt - $P \e[0m"
+(echo "timer" > $l1)
+(echo "80" > $l2)
+(echo "$SNR" >  $l3)
+    elif [ $SNR -le 399 -a $Snr >0 ]
      then
-# sygnal slaby
+# slaby syg
+echo -e "\33[31;40m $Ka $Snr  odczyt - $P \e[0m"
 (echo "timer" > $l1)
 (echo "70" > $l2)
 (echo "3000" >  $l3)
      else
-# brak sygnalu
+# brak syg
 (echo "none" > $l1)
       fi
    let P=$((P + 1 ))
@@ -111,14 +117,13 @@ done
        then
       if  [ $HardM = "0777" -a $N = 1 ]
          then
-echo -e "\t UBIQUITI  sygnalizacja dioda LED"
-
+echo -e "\t\33[33;40m UBIQUITI sygnalizacja dioda LED \e[0m"
 l4="/sys/class/leds/ubnt:orange:link2/trigger"
 l5="/sys/class/leds/ubnt:orange:link2/delay_off"
 l6="/sys/class/leds/ubnt:orange:link2/delay_on"
 (echo "none" > $l4)
 while [ $P -le $R ] ; do
-# czas odczytu
+# czas odcz
 sleep 1
 export Wyn=`iwinfo $inter assoclist | grep "SNR" |cut -d " " -f 9 | cut -d ")" -f 1 | sort`
 # pobranie SNR
@@ -140,21 +145,27 @@ SNR=$((400 -((Snr) ** 2 )))
          then
     Snr=$((Snr * -1 ))
       fi 
-   if [ $SNR -le 350 -a $Snr > 0 ]
+   if [ $SNR -le 250 -a $Snr > 0 ]
       then
-echo -e "$Ka $Snr  odczyt - $P"
-# poziom sygnalu 
+echo -e "\33[32;40m $Ka $Snr  odczyt - $P \e[0m"
 (echo "timer" > $l4)
 (echo "70" > $l5)
 (echo "$SNR" > $l6)
-   elif [ $SNR -le 396 -a $Snr >0 ]
+    elif [ $SNR -le 350 -a $Snr > 0 ]
+      then
+echo -e "\33[32;40m $Ka $Snr  odczyt - $P \e[0m"
+(echo "timer" > $l4)
+(echo "80" > $l5)
+(echo "$SNR" > $l6)
+   elif [ $SNR -le 399 -a $Snr >0 ]
      then
-# sygnal slaby
+# slaby syg
+echo -e "\33[31;40m $Ka $Snr  odczyt - $P \e[0m"
 (echo "timer" > $l4)
 (echo "70" > $l5)
 (echo "3000" > $l6)
      else
-# brak sygnalu
+# brak syg
 (echo "none" > $l4)
       fi
    let P=$((P + 1 ))
@@ -166,17 +177,16 @@ done
        fi
      fi
    fi
- 
-# odczyt bez sygnalozacji
+# odczyt bez LED
     if [ $h = 1 ]
          then
 sleep 3
 clear
-echo -e "\n--------------------------------------------------------------"
-echo " Wskaznik sygnalu do ustawienia kierunku anteny V1.09  SP9FUT"
+echo -e "\n\33[36;40m---------------------------------------------------------------"
+echo " Wskaznik sygnalu do ustawienia kierunku anteny V1.10  SP9FUT"
 echo "       STACJE POLACZONE BEZPOSREDNIO W SIECI HamNet "
-echo " `iwinfo $inter assoclist | grep "SNR"`"
-echo "--------------------------------------------------------------"
+echo "`iwinfo $inter assoclist | grep "SNR"`"
+echo -e "--------------------------------------------------------------- \e[0m"
 while [ $P -le $R ] ; do
 sleep 2
 export Wyn=`iwinfo $inter assoclist | grep "SNR" |cut -d " " -f 9 | cut -d ")" -f 1 | sort`
@@ -193,16 +203,21 @@ Sa=$( echo $Wyn | cut -d " " -f1 )
          then
          Snr=$((Snr * -1 ))
       fi 
-
-     if [ $Snr -gt 1 ]
-         then
-echo -e "$Ka $Snr  odczyt - $P"
-     fi
+      if [ $Snr -ge 13 ]
+        then
+echo -e "\33[32;40m $Ka $Snr  odczyt - $P \e[0m"
+      elif [ $Snr -ge 6 ]
+       then
+echo -e "\33[33;40m $Ka $Snr  odczyt - $P \e[0m"
+      elif [ $Snr >0 ]
+       then
+echo -e "\33[31;40m $Ka $Snr  odczyt - $P \e[0m"
+      fi
      if [ $Snr > 0 ]
          then
       let P=$((P + 1 ))
-
      fi
 done
      fi
-# wersja z 2015.03.14
+
+# wersja z 2015.04.21
